4bcd8c6cfc496f3f9de52651cbeae8a3a62bcaee
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-05-23 16:13:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a project aimed at developing basic back-end skills in node.js and express.
+It follows a [Local Library tutorial on MDN](https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Tutorial_local_library_website), including challenges 
---
+++ models/bookinstance.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var moment = require('moment');
+
+var BookInstanceSchema = new Schema(
+  {
+    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },  //reference to the associated book
+    imprint: { type: String, required: true },
+    due_back: { type: Date, default: Date.now },
+    status: { type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance' },
+  }
+);
+
+//Virtual for bookinstance's URL
+BookInstanceSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return moment(this.due_back).format('MMMM Do, YYYY');
+});
+
+BookInstanceSchema
+.virtual('due_back_for_update')
+.get(function () {
+  return moment(this.due_back).format('YYYY-MM-DD');
+});
+
+//Export model
+module.exports = mongoose.model('BookInstance', BookInstanceSchema);
